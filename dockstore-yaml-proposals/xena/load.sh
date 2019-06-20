e42b01b4bfbc494ffca8cbb20bb7fde58f6de86f
Charles Overbeck <coverbec@ucsc.edu>
2019-06-20 16:44:29 -0700

    Changes to get working with a Dockstore prototype

--- dockstore-yaml-proposals/xena/load.sh
+++ dockstore-yaml-proposals/xena/load.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-for filename in xena/files/*.tsv; do
-    docker exec xena java -jar /ucsc_xena/cavm-0.24.0-standalone.jar --load /root/xena/files/$(basename $filename)
+echo "Loading data into Xena Hub"
+for filename in xena/files/*.{tsv,probemap}; do
+    tsv=$(basename $filename)
+    echo Loading ${tsv}
+    docker exec xena java -jar /ucsc_xena/cavm-0.24.0-standalone.jar --load /root/xena/files/${tsv}
 done
--- dockstore-yaml-proposals/xena/stand_up.sh
+++ dockstore-yaml-proposals/xena/stand_up.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-export parameters_httpPort=${parameters_httpPort:-7222}
-export parameters_httpsPort=${parameters_httpsPort:-7223}
+echo "Starting up Xena Hub"
+export HTTP_PORT=$httpPort
+export HTTPS_PORT=$httpsPort
 docker-compose up -d
 # Wait for server to respond before exiting
-timeout 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:${parameters_httpPort}/ping/)" != "200" ]]; do sleep 5; done' || false
+# Could use timeout, but not available on a Mac
+attempt_counter=0
+sleep_between_attempts=5
+max_time_to_wait_in_seconds=300
+max_attempts=$(expr ${max_time_to_wait_in_seconds} / ${sleep_between_attempts})
+
+echo "Checking that Xena Hub is up"
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:${httpPort}/ping/)" != "200" ]]; do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    sleep ${sleep_between_attempts}
+done
+
+echo "Xena Hub started up"
